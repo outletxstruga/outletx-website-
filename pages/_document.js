4bfa27cb5044cdf4c584e52cf7028500617df569
outletxstruga <edmi9011@gmail.com>
2026-09-04 17:40:58 +0200

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
-  return <Html lang="en" data-scroll-behavior="smooth"><Head><link rel="icon" href="/icon.svg" type="image/svg+xml"/></Head><body><Main/><NextScript/></body></Html>;
+  return <Html lang="en" data-scroll-behavior="smooth"><Head><link rel="icon" href="/icon.svg" type="image/svg+xml"/><link rel="preload" href="/fonts/manrope-variable.ttf" as="font" type="font/ttf" crossOrigin="anonymous"/></Head><body><Main/><NextScript/></body></Html>;
 }
